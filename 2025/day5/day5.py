2b2bfb5cd47512f7b15a746c3e6c8a07259d9782
ChinmayRathod <chinmayrathod26@gmail.com>
2025-12-05 12:54:24 +0530

    Chinmay | Add alternate solution for day 5

--- 2025/day5/day5.py
+++ 2025/day5/day5.py
@@ -33,6 +33,30 @@ def day5():
     part2 = sum((r[1]-r[0]+1) for r in merged_ranges)
     return part1, part2
 
-  
 if __name__ == '__main__':
-    print(day5())         
+    print(day5())      
+
+# crispier solution
+def alternate_day5():
+    with open("input.txt") as f:
+        lines = [line.strip() for line in f]
+
+    blank = lines.index('')
+    ranges = sorted((tuple(map(int, line.split('-'))) for line in lines[:blank]), key=lambda x: x[0])
+    numbers = list(map(int, lines[blank+1:]))
+
+    # merge intervals
+    merged = []
+    start, end = ranges[0]
+    for cs, ce in ranges[1:]:
+        if cs <= end:
+            end = max(end, ce)
+        else:
+            merged.append((start, end))
+            start, end = cs, ce
+    merged.append((start, end))
+
+    part1 = sum(any(s <= num <= e for s, e in merged) for num in numbers)
+    part2 = sum(e - s + 1 for s, e in merged)
+
+    return part1, part2
